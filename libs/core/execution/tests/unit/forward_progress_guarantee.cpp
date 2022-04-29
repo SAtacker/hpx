acc6d558697defca4b9854fab39a42ad6f733ee0
Shreyas Atre <shreyasatre16@gmail.com>
2022-04-29 12:21:57 +0530

    [execution] forward_progress_guarantee tests
    current error:
    https://pastebin.com/ydxVKe6S
    
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

--- libs/core/execution/tests/unit/forward_progress_guarantee.cpp
+++ libs/core/execution/tests/unit/forward_progress_guarantee.cpp
@@ -5,6 +5,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <hpx/execution/queries/get_scheduler.hpp>
+#include <hpx/execution_base/completion_scheduler.hpp>
 #include <hpx/modules/testing.hpp>
 
 #include <exception>
@@ -15,6 +16,35 @@ namespace mylib {
     // CPO
     struct inline_scheduler_0
     {
+        struct sender
+        {
+            template <template <class...> class Tuple,
+                template <class...> class Variant>
+            using value_types = Variant<Tuple<>>;
+
+            template <template <class...> class Variant>
+            using error_types = Variant<std::exception_ptr>;
+
+            static constexpr bool sends_stopped = false;
+
+            struct operation_state
+            {
+                void start() & noexcept {};
+            };
+
+            template <typename R>
+            operation_state connect(R&&) && noexcept
+            {
+                return {};
+            }
+        };
+
+        friend sender tag_invoke(
+            hpx::execution::experimental::schedule_t, inline_scheduler_0)
+        {
+            return {};
+        }
+
         constexpr friend HPX_FORCEINLINE auto tag_invoke(
             hpx::execution::experimental::get_forward_progress_guarantee_t,
             const inline_scheduler_0&) noexcept
@@ -22,32 +52,49 @@ namespace mylib {
             return hpx::execution::experimental::forward_progress_guarantee::
                 concurrent;
         }
+
     } scheduler{};
 
-    // CPO fallback to parallel
+    // CPO fallback to weakly parallel
     struct inline_scheduler_1
     {
-        constexpr friend HPX_FORCEINLINE auto tag_invoke(
-            hpx::execution::experimental::get_forward_progress_guarantee_t,
-            const inline_scheduler_1&) noexcept
-        {
-            return true;
-        }
+    } scheduler_no_impl{};
 
-    } scheduler_return_invalid{};
-
-    // CPO fallback to parallel
+    // CPO fallback to weakly parallel
     struct inline_scheduler_2
     {
-        constexpr friend HPX_FORCEINLINE auto tag_invoke(
-            hpx::execution::experimental::get_forward_progress_guarantee_t,
-            inline_scheduler_2) noexcept
+        // similar to std::execution
+        struct __sender
         {
-            return hpx::execution::experimental::forward_progress_guarantee::
-                concurrent;
-        }
+            using completion_signatures =
+                hpx::execution::experimental::completion_signatures<
+                    hpx::execution::experimental::set_value_t(),
+                    hpx::execution::experimental::set_error_t(
+                        std::exception_ptr)>;
 
-    } scheduler_no_const_ref{};
+            template <class R>
+            friend auto tag_invoke(hpx::execution::experimental::connect_t,
+                __sender, R&& rec) noexcept(std::
+                    is_nothrow_constructible_v<std::remove_cvref_t<R>, R>)
+            {
+                return {(R &&) rec};
+            }
+
+            friend inline_scheduler_2 tag_invoke(
+                hpx::execution::experimental::get_completion_scheduler_t<
+                    hpx::execution::experimental::set_value_t>,
+                __sender) noexcept
+            {
+                return {};
+            }
+        };
+
+        friend __sender tag_invoke(hpx::execution::experimental::schedule_t,
+            const inline_scheduler_2&) noexcept
+        {
+            return {};
+        }
+    } scheduler_tag{};
 
 }    // namespace mylib
 
@@ -58,17 +105,43 @@ int main()
                       scheduler) ==
             hpx::execution::experimental::forward_progress_guarantee::
                 concurrent,
-        "scheduler::fpg should return concurrent");
+        "scheduler should return concurrent");
 
     static_assert(hpx::execution::experimental::get_forward_progress_guarantee(
-                      scheduler_return_invalid) ==
-            hpx::execution::experimental::forward_progress_guarantee::parallel,
-        "CPO should invoke tag_fallback which should return parallel");
+                      scheduler_no_impl) ==
+            hpx::execution::experimental::forward_progress_guarantee::
+                weakly_parallel,
+        "CPO should invoke tag_fallback which should return weakly parallel");
 
     static_assert(hpx::execution::experimental::get_forward_progress_guarantee(
-                      scheduler_no_const_ref) ==
-            hpx::execution::experimental::forward_progress_guarantee::parallel,
-        "CPO should invoke tag_fallback which should return parallel");
+                      scheduler_tag) ==
+            hpx::execution::experimental::forward_progress_guarantee::
+                weakly_parallel,
+        "CPO should invoke tag_fallback which should return weakly parallel");
+
+    {
+        auto tag_invoke_return =
+            hpx::execution::experimental::schedule(scheduler_tag);
+        static_assert(
+            hpx::execution::experimental::get_forward_progress_guarantee(
+                tag_invoke_return) ==
+                hpx::execution::experimental::forward_progress_guarantee::
+                    weakly_parallel,
+            "CPO should invoke tag_fallback which should return weakly "
+            "parallel");
+    }
+
+    {
+        auto tag_invoke_return =
+            hpx::execution::experimental::schedule(scheduler);
+        static_assert(
+            hpx::execution::experimental::get_forward_progress_guarantee(
+                tag_invoke_return) ==
+                hpx::execution::experimental::forward_progress_guarantee::
+                    weakly_parallel,
+            "CPO should invoke tag_fallback which should return weakly "
+            "parallel");
+    }
 
     return hpx::util::report_errors();
 }
