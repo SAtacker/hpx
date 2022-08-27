ac859f4645e7c11fe28e01bd7f35fa7b92703707
Shreyas Atre <shreyasatre16@gmail.com>
2022-08-28 03:04:43 +0530

    [P2300] fix:Shift connect_result_t from senders to completion_signatures
    - This is done so to avoid build errors

--- libs/core/execution_base/include/hpx/execution_base/sender.hpp
+++ libs/core/execution_base/include/hpx/execution_base/sender.hpp
@@ -181,13 +181,4 @@ namespace hpx { namespace execution { namespace experimental {
     template <typename S>
     using schedule_result_t = hpx::util::invoke_result_t<schedule_t, S>;
 
-    template <typename S, typename R>
-    using connect_result_t = hpx::util::invoke_result_t<connect_t, S, R>;
-
-    namespace detail {
-        // Dummy type used in place of a scheduler if none is given
-        struct no_scheduler
-        {
-        };
-    }    // namespace detail
-}}}      // namespace hpx::execution::experimental
+}}}    // namespace hpx::execution::experimental
