e023a98c2148699525f80a25e6d721404eaac8dd
Shreyas Atre <shreyasatre16@gmail.com>
2022-11-09 01:26:58 +0530

    [Senders/Receivers] Experiment with Senders Receivers algorithms
    - Use S/R for 1d stencil
    
    Signed-off-by: Shreyas Atre <shreyasatre16@gmail.com>

---
+++ examples/1d_stencil/1d_stencil_3_sr.cpp
@@ -0,0 +1,245 @@
+//  Copyright (c) 2022 Shreyas Atre
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// This example builds on example four. Uses S/R replacing futures from that
+// example. Compared to example two this code runs much more efficiently. It
+// allows for changing the amount of work executed in one HPX thread which
+// enables tuning the performance for the optimal grain size of the
+// computation. This example is still fully local but demonstrates nice
+// scalability on SMP machines.
+
+#include <hpx/local/algorithm.hpp>
+#include <hpx/local/chrono.hpp>
+#include <hpx/local/execution.hpp>
+#include <hpx/local/future.hpp>
+#include <hpx/local/init.hpp>
+#include <hpx/modules/iterator_support.hpp>
+
+#if !defined(HPX_HAVE_CXX17_SHARED_PTR_ARRAY)
+#include <boost/shared_array.hpp>
+#else
+#include <memory>
+#endif
+
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include "print_time_results.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Command-line variables
+bool header = true;    // print csv heading
+double k = 0.5;        // heat transfer coefficient
+double dt = 1.;        // time step
+double dx = 1.;        // grid spacing
+
+inline std::size_t idx(std::size_t i, int dir, std::size_t size)
+{
+    if (i == 0 && dir == -1)
+        return size - 1;
+    if (i == size - 1 && dir == +1)
+        return 0;
+
+    HPX_ASSERT((i + dir) < size);
+
+    return i + dir;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Our partition_data data type
+struct partition_data
+{
+    partition_data(std::size_t size = 0)
+      : data_(size)
+    {
+    }
+
+    partition_data(std::size_t size, double initial_value)
+      : data_(size)
+    {
+        double base_value = double(initial_value * size);
+        for (std::size_t i = 0; i != size; ++i)
+            data_[i] = base_value + double(i);
+    }
+
+    double& operator[](std::size_t idx)
+    {
+        return data_[idx];
+    }
+    double operator[](std::size_t idx) const
+    {
+        return data_[idx];
+    }
+
+    std::size_t size() const
+    {
+        return data_.size();
+    }
+
+private:
+    std::vector<double> data_;
+};
+
+std::ostream& operator<<(std::ostream& os, partition_data const& c)
+{
+    os << "{";
+    for (std::size_t i = 0; i != c.size(); ++i)
+    {
+        if (i != 0)
+            os << ", ";
+        os << c[i];
+    }
+    os << "}";
+    return os;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+struct stepper
+{
+    // Our data for one time step
+    typedef partition_data partition;
+    typedef std::vector<partition> space;
+
+    std::vector<space> U;
+
+    // Our operator
+    static inline double heat(double left, double middle, double right)
+    {
+        return middle + (k * dt / (dx * dx)) * (left - 2 * middle + right);
+    }
+
+    // The partitioned operator, it invokes the heat operator above on all
+    // elements of a partition.
+    static partition_data heat_part(partition_data const& left,
+        partition_data const& middle, partition_data const& right)
+    {
+        std::size_t size = middle.size();
+        partition_data next(size);
+
+        next[0] = heat(left[size - 1], middle[0], middle[1]);
+
+        for (std::size_t i = 1; i != size - 1; ++i)
+            next[i] = heat(middle[i - 1], middle[i], middle[i + 1]);
+
+        next[size - 1] = heat(middle[size - 2], middle[size - 1], right[0]);
+
+        return next;
+    }
+
+    void fill(std::size_t np, std::size_t nx)
+    {
+        U.resize(2);
+
+        // U[t][i] is the state of position i at time t.
+        for (space& s : U)
+            s.resize(np);
+
+        // Initial conditions: f(0, i) = i
+        for (std::size_t i = 0; i != np; ++i)
+            U[0][i] = partition_data(nx, double(i));
+    }
+
+    // do all the work on 'np' partitions, 'nx' data points each, for 'nt'
+    // time steps
+    space do_work(std::size_t np, std::size_t nt)
+    {
+        // Actual time step loop
+        for (std::size_t t = 0; t != nt; ++t)
+        {
+            space const& current = U[t % 2];
+            space& next = U[(t + 1) % 2];
+
+            for (std::size_t i = 0; i != np; ++i)
+            {
+                auto c1 =
+                    hpx::execution::experimental::just(current[idx(i, -1, np)]);
+                auto c2 = hpx::execution::experimental::just(current[i]);
+                auto c3 =
+                    hpx::execution::experimental::just(current[idx(i, +1, np)]);
+                next[i] = hpx::get<0>(hpx::this_thread::experimental::sync_wait(
+                    hpx::execution::experimental::when_all(c1, c2, c3) |
+                    hpx::execution::experimental::then(
+                        [&](auto a, auto b, auto c) {
+                            return stepper::heat_part(a, b, c);
+                        })).value());
+            }
+        }
+
+        // Return the solution at time-step 'nt'.
+        return U[nt % 2];
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+int hpx_main(hpx::program_options::variables_map& vm)
+{
+    std::uint64_t np = vm["np"].as<std::uint64_t>();    // Number of partitions.
+    std::uint64_t nx =
+        vm["nx"].as<std::uint64_t>();    // Number of grid points.
+    std::uint64_t nt = vm["nt"].as<std::uint64_t>();    // Number of steps.
+
+    if (vm.count("no-header"))
+        header = false;
+
+    // Create the stepper object
+    stepper step;
+
+    // Fill with initial data
+    step.fill(np, nx);
+
+    // Measure execution time.
+    std::uint64_t t = hpx::chrono::high_resolution_clock::now();
+
+    // Execute nt time steps on nx grid points and print the final solution.
+    stepper::space solution = step.do_work(np, nt);
+
+    std::uint64_t elapsed = hpx::chrono::high_resolution_clock::now() - t;
+
+    // Print the final solution
+    if (vm.count("results"))
+    {
+        for (std::size_t i = 0; i != np; ++i)
+            std::cout << "U[" << i << "] = " << solution[i] << std::endl;
+    }
+
+    std::uint64_t const os_thread_count = hpx::get_os_thread_count();
+    print_time_results(os_thread_count, elapsed, nx, np, nt, header);
+
+    return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    namespace po = hpx::program_options;
+
+    po::options_description desc_commandline;
+    // clang-format off
+    desc_commandline.add_options()
+        ("results", "print generated results (default: false)")
+        ("nx", po::value<std::uint64_t>()->default_value(10),
+         "Local x dimension (of each partition)")
+        ("nt", po::value<std::uint64_t>()->default_value(45),
+         "Number of time steps")
+        ("np", po::value<std::uint64_t>()->default_value(10),
+         "Number of partitions")
+        ("k", po::value<double>(&k)->default_value(0.5),
+         "Heat transfer coefficient (default: 0.5)")
+        ("dt", po::value<double>(&dt)->default_value(1.0),
+         "Timestep unit (default: 1.0[s])")
+        ("dx", po::value<double>(&dx)->default_value(1.0),
+         "Local x dimension")
+        ( "no-header", "do not print out the csv header row")
+    ;
+    // clang-format on
+
+    // Initialize and run HPX
+    hpx::local::init_params init_args;
+    init_args.desc_cmdline = desc_commandline;
+
+    return hpx::local::init(hpx_main, argc, argv, init_args);
+}
